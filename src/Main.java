7efd172a64762131902b1005d13a5d061df4234d
Aleksey Kaschenko <aleksey.kaschenko@gmail.com>
2022-10-19 10:08:45 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int ticketCost;
+        int bonusMileValue;
+        int milesPerTicket;
+        ticketCost = 1500;
+        bonusMileValue = 20;
+        milesPerTicket = ticketCost / bonusMileValue;
+       System.out.println(milesPerTicket + " - количество начисленных миль за купленный билет.");
+
+    }
+}
