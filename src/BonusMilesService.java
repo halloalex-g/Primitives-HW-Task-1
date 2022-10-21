5291001cba5f93680b470a1333aec3ef82143881
Aleksey Kaschenko <aleksey.kaschenko@gmail.com>
2022-10-21 17:32:18 +0600

    Modified miles count HWOOP Task1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+   public int calculate(int cost, int price) {
+       int result;
+       result = price / cost;
+       return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketCost;
-        int bonusMileValue;
-        int milesPerTicket;
-        ticketCost = 1500;
-        bonusMileValue = 20;
-        milesPerTicket = ticketCost / bonusMileValue;
-        System.out.println(milesPerTicket + " - количество начисленных миль за купленный билет.");
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(20, price);
+        System.out.println(miles);
     }
 }
