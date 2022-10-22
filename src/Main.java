92163ba8a02fa6bc5886cc61792f6a44f822316a
Aleksey Kaschenko <aleksey.kaschenko@gmail.com>
2022-10-22 11:15:59 +0600

    Added var cost

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = service.calculate(20, price);
+        int cost = 20;
+        int miles = service.calculate(cost, price);
         System.out.println(miles);
     }
 }
